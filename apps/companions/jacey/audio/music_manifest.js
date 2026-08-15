8362df9ef4e8d738e86b8cfcac4df4232bc2c0b4
AquaFia <aquafia1247@gmail.com>
2026-08-15 12:22:35 -0400

    jacey_v7_3_3

---
+++ apps/companions/jacey/audio/music_manifest.js
@@ -0,0 +1,12 @@
+window.CompanionMusicManifest = {
+  "jace": [
+    "Kozoro - Curious (feat. Noctilucent).mp3",
+    "More Than A Mystery - Meg Donnelly.mp3"
+  ],
+  "mao": [
+    "LAPUTA - Masquerade (feat. Noctilucent).mp3"
+  ],
+  "naoya": [
+    "Psycho Crooks - Respawn.mp3"
+  ]
+};
